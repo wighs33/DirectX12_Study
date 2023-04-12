adde450d37f19937c4c9538eb91e5b8203950fec
wighs33 <genji@kakao.com>
2023-04-12 20:15:18 +0900

    메인 파일 분리

---
+++ DirectX12_Study/Sources/main.cpp
@@ -0,0 +1,24 @@
+#include "InitDirect3DApp.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+    PSTR cmdLine, int showCmd)
+{
+    // 디버그 빌드에서는 실행지점 메모리 점검 기능을 켠다.
+#if defined(DEBUG) | defined(_DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+    try
+    {
+        InitDirect3DApp theApp(hInstance);
+        if (!theApp.Initialize())
+            return 0;
+
+        return theApp.Run();
+    }
+    catch (DxException& e)
+    {
+        MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+        return 0;
+    }
+}
